a89c856c5906a0ea5dee0c43206f82f942108e8a
alanmc <alanmc@swingvine.com>
2010-09-17 12:09:36 -0700

    Add in appirater review dialog. In the future we'll integrate this more cleanly, but just dump it in for now
    Signed-off-by: alanmc <alanmc@swingvine.com>

--- iphone/Classes/TiApp.mm
+++ iphone/Classes/TiApp.mm
@@ -3,6 +3,36 @@
  * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
+ * 
+ * WARNING: This is generated code. Modify at your own risk and without support.
+ */
+
+/*
+ This file is part of Appirater.
+ 
+ Copyright (c) 2010, Arash Payan
+ All rights reserved.
+ 
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+ 
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
  */
 #include <stdio.h>
 #include <execinfo.h>
@@ -16,6 +46,8 @@
 #import "ImageLoader.h"
 #import <QuartzCore/QuartzCore.h>
 #import <AVFoundation/AVFoundation.h>
+#import <SystemConfiguration/SCNetworkReachability.h>
+#include <netinet/in.h>
 
 #import <libkern/OSAtomic.h>
 
@@ -81,6 +113,187 @@ void MyUncaughtExceptionHandler(NSException *exception)
 	//TODO - attempt to report the exception
 	insideException=NO;
 }
+
+NSString *const kAppiraterLaunchDate				= @"kAppiraterLaunchDate";
+NSString *const kAppiraterLaunchCount				= @"kAppiraterLaunchCount";
+NSString *const kAppiraterCurrentVersion			= @"kAppiraterCurrentVersion";
+NSString *const kAppiraterRatedCurrentVersion		= @"kAppiraterRatedCurrentVersion";
+NSString *const kAppiraterDeclinedToRate			= @"kAppiraterDeclinedToRate";
+
+NSString *templateReviewURL = @"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
+
+@interface Appirater (hidden)
+- (BOOL)connectedToNetwork;
+@end
+
+@implementation Appirater (hidden)
+
+- (BOOL)connectedToNetwork {
+    // Create zero addy
+    struct sockaddr_in zeroAddress;
+    bzero(&zeroAddress, sizeof(zeroAddress));
+    zeroAddress.sin_len = sizeof(zeroAddress);
+    zeroAddress.sin_family = AF_INET;
+	
+    // Recover reachability flags
+    SCNetworkReachabilityRef defaultRouteReachability = SCNetworkReachabilityCreateWithAddress(NULL, (struct sockaddr *)&zeroAddress);
+    SCNetworkReachabilityFlags flags;
+	
+    BOOL didRetrieveFlags = SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags);
+    CFRelease(defaultRouteReachability);
+	
+    if (!didRetrieveFlags)
+    {
+        NSLog(@"Error. Could not recover network reachability flags");
+        return NO;
+    }
+	
+    BOOL isReachable = flags & kSCNetworkFlagsReachable;
+    BOOL needsConnection = flags & kSCNetworkFlagsConnectionRequired;
+	BOOL nonWiFi = flags & kSCNetworkReachabilityFlagsTransientConnection;
+	
+	NSURL *testURL = [NSURL URLWithString:@"http://www.apple.com/"];
+	NSURLRequest *testRequest = [NSURLRequest requestWithURL:testURL  cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:20.0];
+	NSURLConnection *testConnection = [[NSURLConnection alloc] initWithRequest:testRequest delegate:self];
+	
+    return ((isReachable && !needsConnection) || nonWiFi) ? (testConnection ? YES : NO) : NO;
+}
+
+@end
+
+
+@implementation Appirater
+
++ (void)appLaunched {
+	Appirater *appirater = [[Appirater alloc] init];
+	[NSThread detachNewThreadSelector:@selector(_appLaunched) toTarget:appirater withObject:nil];
+}
+
+- (void)_appLaunched {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	if (APPIRATER_DEBUG)
+	{
+		[self performSelectorOnMainThread:@selector(showPrompt) withObject:nil waitUntilDone:NO];
+		
+		return;
+	}
+	
+	BOOL willShowPrompt = NO;
+	
+	// get the app's version
+	NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleVersionKey];
+	
+	// get the version number that we've been tracking
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	NSString *trackingVersion = [userDefaults stringForKey:kAppiraterCurrentVersion];
+	if (trackingVersion == nil)
+	{
+		trackingVersion = version;
+		[userDefaults setObject:version forKey:kAppiraterCurrentVersion];
+	}
+	
+	if (APPIRATER_DEBUG)
+		NSLog(@"APPIRATER Tracking version: %@", trackingVersion);
+	
+	if ([trackingVersion isEqualToString:version])
+	{
+		// get the launch date
+		NSTimeInterval timeInterval = [userDefaults doubleForKey:kAppiraterLaunchDate];
+		if (timeInterval == 0)
+		{
+			timeInterval = [[NSDate date] timeIntervalSince1970];
+			[userDefaults setDouble:timeInterval forKey:kAppiraterLaunchDate];
+		}
+		
+		NSTimeInterval secondsSinceLaunch = [[NSDate date] timeIntervalSinceDate:[NSDate dateWithTimeIntervalSince1970:timeInterval]];
+		double secondsUntilPrompt = 60 * 60 * 24 * DAYS_UNTIL_PROMPT;
+		
+		// get the launch count
+		int launchCount = [userDefaults integerForKey:kAppiraterLaunchCount];
+		launchCount++;
+		[userDefaults setInteger:launchCount forKey:kAppiraterLaunchCount];
+		if (APPIRATER_DEBUG)
+			NSLog(@"APPIRATER Launch count: %d", launchCount);
+		
+		// have they previously declined to rate this version of the app?
+		BOOL declinedToRate = [userDefaults boolForKey:kAppiraterDeclinedToRate];
+		
+		// have they already rated the app?
+		BOOL ratedApp = [userDefaults boolForKey:kAppiraterRatedCurrentVersion];
+		
+		if (secondsSinceLaunch > secondsUntilPrompt &&
+			launchCount > LAUNCHES_UNTIL_PROMPT &&
+			!declinedToRate &&
+			!ratedApp)
+		{
+			if ([self connectedToNetwork])	// check if they can reach the app store
+			{
+				willShowPrompt = YES;
+				[self performSelectorOnMainThread:@selector(showPrompt) withObject:nil waitUntilDone:NO];
+			}
+		}
+	}
+	else
+	{
+		// it's a new version of the app, so restart tracking
+		[userDefaults setObject:version forKey:kAppiraterCurrentVersion];
+		[userDefaults setDouble:[[NSDate date] timeIntervalSince1970] forKey:kAppiraterLaunchDate];
+		[userDefaults setInteger:1 forKey:kAppiraterLaunchCount];
+		[userDefaults setBool:NO forKey:kAppiraterRatedCurrentVersion];
+		[userDefaults setBool:NO forKey:kAppiraterDeclinedToRate];
+	}
+
+	
+	[userDefaults synchronize];
+	if (!willShowPrompt)
+		[self autorelease];
+	
+	[pool release];
+}
+
+- (void)showPrompt {
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:APPIRATER_MESSAGE_TITLE
+														message:APPIRATER_MESSAGE
+													   delegate:self
+											  cancelButtonTitle:APPIRATER_CANCEL_BUTTON
+											  otherButtonTitles:APPIRATER_RATE_BUTTON, APPIRATER_RATE_LATER, nil];
+	[alertView show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	
+	switch (buttonIndex) {
+		case 0:
+		{
+			// they don't want to rate it
+			[userDefaults setBool:YES forKey:kAppiraterDeclinedToRate];
+			break;
+		}
+		case 1:
+		{
+			// they want to rate it
+			NSString *reviewURL = [templateReviewURL stringByReplacingOccurrencesOfString:@"APP_ID" withString:[NSString stringWithFormat:@"%d", APPIRATER_APP_ID]];
+			[[UIApplication sharedApplication] openURL:[NSURL URLWithString:reviewURL]];
+			
+			[userDefaults setBool:YES forKey:kAppiraterRatedCurrentVersion];
+			break;
+		}
+		case 2:
+			// remind them later
+			break;
+		default:
+			break;
+	}
+	
+	[userDefaults synchronize];
+	
+	[alertView release];
+	[self release];
+}
+
+@end
 
 @implementation TiApp
 
@@ -245,7 +458,7 @@ void MyUncaughtExceptionHandler(NSException *exception)
 
 - (void)boot
 {
-	NSLog(@"[INFO] %@/%@ (%s.__GITHASH__)",TI_APPLICATION_NAME,TI_APPLICATION_VERSION,TI_VERSION_STR);
+	NSLog(@"[INFO] %@/%@ (%s.c51a57)",TI_APPLICATION_NAME,TI_APPLICATION_VERSION,TI_VERSION_STR);
 	
 	sessionId = [[TiUtils createUUID] retain];
 
@@ -331,7 +544,8 @@ void MyUncaughtExceptionHandler(NSException *exception)
 	}
 	
 	[self boot];
-	
+
+	[Appirater appLaunched];	
 	return YES;
 }
 
@@ -583,8 +797,8 @@ void MyUncaughtExceptionHandler(NSException *exception)
 		UIDevice *currentDevice = [UIDevice currentDevice];
 		NSString *currentLocaleIdentifier = [[NSLocale currentLocale] localeIdentifier];
 		NSString *currentDeviceInfo = [NSString stringWithFormat:@"%@/%@; %@; %@;",[currentDevice model],[currentDevice systemVersion],[currentDevice systemName],currentLocaleIdentifier];
-		NSString *kTitaniumUserAgentPrefix = [NSString stringWithFormat:@"%s%s%s %s%s","Appc","eler","ator","Tita","nium"];
-		userAgent = [[NSString stringWithFormat:@"%@/%s (%@)",kTitaniumUserAgentPrefix,TI_VERSION_STR,currentDeviceInfo] retain];
+		NSString *kSwingVineUserAgentPrefix = [NSString stringWithFormat:@"%s%s%s %s%s","Appc","eler","ator","Tita","nium"];
+		userAgent = [[NSString stringWithFormat:@"%@/%s (%@)",kSwingVineUserAgentPrefix,TI_VERSION_STR,currentDeviceInfo] retain];
 	}
 	return userAgent;
 }
